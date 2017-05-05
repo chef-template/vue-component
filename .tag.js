790b7910eafe9487b6156e61fc13bb208b1c8dd1
Jerret321 <321jiangtao@gmail.com>
2017-05-05 16:53:25 +0800

    add naming tag

--- .tag.js
+++ .tag.js
@@ -1,13 +1,19 @@
 var spawnSync = require('child_process').spawnSync
-var version = require('./package.json').version
+var package = require('./package.json')
+
+var version = package.version
+var name = package.name
+
 var args = process.argv.slice(2)
 var action = args.pop()
+var message = args.pop() || `${name} ${version}`
 
 if (!/\d+\.\d+\.\d+/.test(version)) {
     return console.log('\x1b[31m', 'package version非法')
 }
+
 switch (action) {
-    
+
     case 'add:local':
         addLocalTag(version)
         break
@@ -15,14 +21,19 @@ switch (action) {
     case 'add:remote':
         addRemoteTag(version)
         break
-    
+
+    case 'add':
+        addLocalTag(version)
+        addRemoteTag(version)
+        break
+
     default:
         addLocalTag(version)
         addRemoteTag(version)
 }
 
 function addLocalTag(tag) {
-    let local = spawnSync('git', ['tag', tag])
+    let local = spawnSync('git', ['tag', '-a', tag, '-v', message])
     logInfo(local.stdout.toString())
     logErr(local.stderr.toString())
 }
